c55e589bc5b135f5b8300dfd1b1c6eede9fc44cb
Crithian Pelaez <cristhianpelaez13@gmail.com>
2020-01-14 12:44:39 -0500

    up project

---
+++ src/main/java/com/zealep/dental/app/DentalbackendApplication.java
@@ -0,0 +1,13 @@
+package com.zealep.dental.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zealep/dental/app/DentalbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zealep.dental.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
